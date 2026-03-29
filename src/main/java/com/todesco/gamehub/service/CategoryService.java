019552c51f79bfc562235fa26545e7fe60272795
giotodsco <todescogiovanni18@gmail.com>
2026-03-29 20:42:43 -0300

    feat: create methods list all, list per id, create and delete

--- src/main/java/com/todesco/gamehub/service/CategoryService.java
+++ src/main/java/com/todesco/gamehub/service/CategoryService.java
@@ -1,7 +1,31 @@
 package com.todesco.gamehub.service;
 
+import com.todesco.gamehub.entity.Category;
+import com.todesco.gamehub.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    public List<Category> listAllCategory(){
+        return categoryRepository.findAll();
+    }
+
+    public Category createCategory(Category category){
+        return categoryRepository.save(category);
+    }
+
+    public Category listPerId(Long id){
+        return categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public void deletePerId(Long id){
+        categoryRepository.deleteById(id);
+    }
 }
