c0598673c8f9c123fa2a105aa450ff4ca3451315
giotodsco <todescogiovanni18@gmail.com>
2026-03-29 20:42:55 -0300

    feat: create endpoints list all, list per id, create and delete

--- src/main/java/com/todesco/gamehub/controller/CategoryController.java
+++ src/main/java/com/todesco/gamehub/controller/CategoryController.java
@@ -1,10 +1,41 @@
 package com.todesco.gamehub.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.todesco.gamehub.entity.Category;
+import com.todesco.gamehub.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/gamehub/category")
-
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/find/all")
+    public ResponseEntity<List<Category>> listAllCategory(){
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.listAllCategory());
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Category> listPerId(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.listPerId(id));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category){
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.createCategory(category));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id){
+        categoryService.deletePerId(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+
 }
