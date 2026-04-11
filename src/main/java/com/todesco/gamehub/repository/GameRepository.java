caa23fb69e58e0168668aaa2095763a11849960a
giotodsco <todescogiovanni18@gmail.com>
2026-04-11 12:16:52 -0300

    feat: add method findGameByCategories

--- src/main/java/com/todesco/gamehub/repository/GameRepository.java
+++ src/main/java/com/todesco/gamehub/repository/GameRepository.java
@@ -1,7 +1,11 @@
 package com.todesco.gamehub.repository;
 
+import com.todesco.gamehub.entity.Category;
 import com.todesco.gamehub.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GameRepository extends JpaRepository<Game, Long> {
+    List<Game> findGameByCategoriesIn(List<Category> categories);
 }
