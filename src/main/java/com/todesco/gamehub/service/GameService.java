1de61589adc82aac190541f5f27c92413463ee6e
giotodsco <todescogiovanni18@gmail.com>
2026-04-11 12:17:44 -0300

    feat: add method findGameStore and UpdateGame

--- src/main/java/com/todesco/gamehub/service/GameService.java
+++ src/main/java/com/todesco/gamehub/service/GameService.java
@@ -1,9 +1,11 @@
 package com.todesco.gamehub.service;
 
 import com.todesco.gamehub.dtos.request.GameRequest;
+import com.todesco.gamehub.dtos.response.CategoryResponse;
 import com.todesco.gamehub.dtos.response.GameResponse;
 import com.todesco.gamehub.entity.Category;
 import com.todesco.gamehub.entity.Game;
+import com.todesco.gamehub.entity.GameStore;
 import com.todesco.gamehub.mapper.GameMapper;
 import com.todesco.gamehub.repository.GameRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +20,16 @@ public class GameService {
 
     private final GameRepository gameRepository;
     private final CategoryService categoryService;
+    private final GameStoreService gameStoreService;
 
     public GameResponse createGame(GameRequest gameRequest){
         Game game = GameMapper.toGame(gameRequest);
 
         List<Category> validatedCategories = findCategories(game.getCategories());
         game.setCategories(validatedCategories);
+
+        List<GameStore> validatedGameStore = findGamestore(game.getGameStores());
+        game.setGameStores(validatedGameStore);
 
         gameRepository.save(game);
         return GameMapper.toResponse(game);
@@ -52,9 +58,43 @@ public class GameService {
                 .toList();
     }
 
+    public List<GameStore> findGamestore(List<GameStore> gameStores){
+        return gameStores.stream()
+                .map(gameStore -> gameStoreService.listEntityPerId(gameStore.getId()))
+                .toList();
+    }
 
+    public GameResponse updateGame(Long id, GameRequest request){
+        Game game = gameRepository.findById(id).
+                orElseThrow(() -> new RuntimeException("Game not found"));
+        Game games = GameMapper.toGame(request);
 
+        List<Category> categories =
+                findCategories(games.getCategories());
 
+        List<GameStore> gameStores =
+                findGamestore(games.getGameStores());
 
+        game.setTitle(games.getTitle());
+        game.setDescription(games.getDescription());
+        game.setPrice(games.getPrice());
+        game.setReleaseDate(games.getReleaseDate());
 
+        game.getGameStores().clear();
+        game.getGameStores().addAll(gameStores);
+
+        game.getCategories().clear();
+        game.getCategories().addAll(categories);
+
+        gameRepository.save(game);
+
+        return GameMapper.toResponse(game);
+    }
+
+    public List<GameResponse> listPerCategory(Long id){
+        return gameRepository.findGameByCategoriesIn(List.of(Category.builder().id(id).build()))
+                .stream()
+                .map(GameMapper::toResponse)
+                .toList();
+    }
 }
