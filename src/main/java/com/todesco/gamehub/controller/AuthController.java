1e68cda32d6cc38b591c4ed0b38bc0b14ce3d8b7
giotodsco <todescogiovanni18@gmail.com>
2026-04-12 14:01:26 -0300

    feat: add auth controller with register and login endpoints

---
+++ src/main/java/com/todesco/gamehub/controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.todesco.gamehub.controller;
+
+import com.todesco.gamehub.config.TokenConfig;
+import com.todesco.gamehub.dtos.request.LoginRequest;
+import com.todesco.gamehub.dtos.request.UserRequest;
+import com.todesco.gamehub.dtos.response.LoginResponse;
+import com.todesco.gamehub.dtos.response.UserResponse;
+import com.todesco.gamehub.entity.User;
+import com.todesco.gamehub.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gamehub/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenConfig tokenConfig;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> registerUser(@RequestBody UserRequest userRequest){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.createUser(userRequest));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest){
+        UsernamePasswordAuthenticationToken userAndPass =
+                new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password());
+
+        Authentication authentication = authenticationManager.authenticate(userAndPass);
+
+        User user = (User) authentication.getPrincipal();
+
+        String token = tokenConfig.generateToken(user);
+
+        return ResponseEntity.ok(new LoginResponse(token));
+
+    }
+}
